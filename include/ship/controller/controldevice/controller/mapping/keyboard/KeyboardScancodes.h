0ad2ae0a3ae7e9a95e002ef0edec0a6d2d974be9
doctorjelly <235556600+doctorjelly@users.noreply.github.com>
2026-08-16 23:06:16 -0400

    fix: avoid per-translation-unit mouse name strings

--- include/ship/controller/controldevice/controller/mapping/keyboard/KeyboardScancodes.h
+++ include/ship/controller/controldevice/controller/mapping/keyboard/KeyboardScancodes.h
@@ -140,9 +140,10 @@ typedef enum WheelDirection {
 } WheelDirection;
 
 #ifdef __cplusplus
-static std::string mouseBtnNames[7] = { "MouseLeft",    "MouseMiddle",     "MouseRight",       "MouseBackward",
-                                        "MouseForward", "MOUSE_BTN_COUNT", "MOUSE_BTN_UNKNOWN" };
-static std::string wheelDirectionNames[6] = { "LUS_WHEEL_NONE", "WheelLeft", "WheelRight",
-                                              "WheelUp",        "WheelDown", "LUS_WHEEL_UNKNOWN" };
+inline constexpr const char* mouseBtnNames[7] = { "MouseLeft",    "MouseMiddle",     "MouseRight",
+                                                  "MouseBackward", "MouseForward",    "MOUSE_BTN_COUNT",
+                                                  "MOUSE_BTN_UNKNOWN" };
+inline constexpr const char* wheelDirectionNames[6] = { "LUS_WHEEL_NONE", "WheelLeft", "WheelRight",
+                                                        "WheelUp",        "WheelDown", "LUS_WHEEL_UNKNOWN" };
 } // namespace Ship
 #endif
